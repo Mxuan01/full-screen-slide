f0b099194bca5eac77d7c329795e14476d258759
zhangkang <ike255789@qq.com>
2017-06-27 22:16:51 +0800

    update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var wrapper = $('.wrapper');
 
 var toNextPage = function(e) {
+  
   var curParent = $(e.target).parents('.page');
 
   setTimeout(function(){
@@ -11,15 +12,22 @@ var toNextPage = function(e) {
 }
 
 wrapper.on('click', '.next-btn', function(e){
+
   toNextPage(e)
 }).on('click', '.lottery_btn', function(e){
+
   var rotate = 'rotateZ(' + (360*4+45) + 'deg)';
+  // var rotate = 'rotate3d(0, 0, 1, ' + (360*4+45) + 'deg)';
 
   $(".pointer").css({
 		'-webkit-transform': rotate,
 		'transform': rotate
-	}).on('transitionend', function(){
+	})
+  .on('transitionend', function(){
     toNextPage(e)
   });
 
+  // setTimeout(function(){
+  //   toNextPage(e)
+  // }, 2200)
 })
